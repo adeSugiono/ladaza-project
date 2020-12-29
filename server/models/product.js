2faf1a6c52bba08fadff2a14f6d0586d72f63a01
adeSugiono <adesugiono354@gmail.com>
2020-12-29 17:15:27 +0700

    firest commit

---
+++ server/models/product.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const dataSchema = new mongoose.Schema({
+    id: Number,
+    title: String,
+    brand: String,
+    price: Number,
+    quantity: Number,
+    category: String
+
+});
+
+module.exports = mongoose.model('Product', dataSchema);
---
+++ server/routes/product.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var router = express.Router();
+var Product = require('../models/product');
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  Product.find({}, (err, data) => {
+    res.status(200).json(data)
+  })
+});
+
+// add product
+router.post('/', function (req, res, next) {
+  const { id, title, brand, price, quantity, description, category } = req.body;
+  Product.create({ id, title, brand, price, quantity, description, category }, function (err, data) {
+    res.status(201).json({
+      status: "SUCCESS",
+      data: data
+    });
+  })
+})
+
+
+// edit product
+router.put('/:id', function (req, res, next) {
+  const { title, brand, price, quantity, description, category } = req.body;
+  Product.findOneAndUpdate({ id: Number(req.params.id) }, {
+    price, title, brand, quantity, description, category
+  }, { new: true }, function (err, data) {
+    res.status(201).json({
+      status: "SUCCESS",
+      data: data
+    });
+  })
+})
+
+// delete product
+router.delete('/:id', function (req, res, next) {
+  Product.findOneAndRemove({ id: Number(req.params.id) }, function (err, data) {
+    res.status(201).json({
+      status: "SUCCESS",
+      data: data
+    });
+  })
+})
+
+
+module.exports = router;
+
