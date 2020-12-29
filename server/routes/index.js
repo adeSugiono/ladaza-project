a9955af3de017c4de2389cbbcbdee8926cb07925
albajiligadingp <albajiligadingp313@gmail.com>
2020-12-29 19:57:37 +0700

    render ejs done

--- server/routes/index.js
+++ server/routes/index.js
@@ -11,28 +11,28 @@ router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Login' });
 });
 
-router.post('/login', function(req, res, next) {
-  Admin.findOne({ email: req.body.email }, (err, data) => {
-    if (err) return res.redirect('/login');
-  })
-})
+// router.post('/login', function(req, res, next) {
+//   Admin.findOne({ email: req.body.email }, (err, data) => {
+//     if (err) return res.redirect('/login');
+//   })
+// })
 
 // Admin Register
 router.get('/register', function(req, res, next) {
   res.render('register', { title: 'Register' });
 });
 
-router.post('/register', function(req, res, next) {
+// router.post('/register', function(req, res, next) {
 
-});
+// });
 
 // Admin Reset Password
 router.get('/reset', function(req, res, next) {
   res.render('reset', { title: 'Reset' });
 });
 
-router.post('/reset', function(req, res, next) {
+// router.post('/reset', function(req, res, next) {
   
-});
+// });
 
 module.exports = router;
